9776da1413a19af88e5c296cfdd9979378153ce5
idrgn <37042855+idrgn@users.noreply.github.com>
2023-06-19 18:20:47 +0200

    Added more tests

--- src/tests.py
+++ src/tests.py
@@ -1,16 +1,24 @@
+import os
+
 from data import resource_path
 from param.param import Param
 from settings.settings import Settings
 
 settings = Settings(open(resource_path("res/settings.txt")).readlines())
 param = Param(None, settings)
-
+directory = "res/base_params/"
 
 with open(resource_path("res/base_params/classparam"), "rb") as file:
     data = file.read()
     param.load_from_data(data)
-
+    print("Checking that classparam has 5 sections")
     assert param.sections == 5
 
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    with open(resource_path(f), "rb") as file:
+        data = file.read()
+        print(f"Loading param file: {filename}")
+        param.load_from_data(data)
 
 print("Tests passed")
