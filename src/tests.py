b67d13451cf6f31d42681b3d79de6a51e850e525
‎Maikel <37042855+idrgn@users.noreply.github.com>
2023-07-14 08:24:16 +0200

    Updated tests for new settings format

--- src/tests.py
+++ src/tests.py
@@ -4,7 +4,23 @@ from data import resource_path
 from param.param import Param
 from settings.settings import Settings
 
-settings = Settings(open(resource_path("res/settings.txt")).readlines())
+# Create settings
+settings = Settings()
+
+# Add the enums from msg files
+directory = resource_path("res/msg/")
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    with open(resource_path(f), "rb") as file:
+        data = file.read()
+        settings.add_enum_from_msg(os.path.splitext(filename)[0], data)
+
+# Load other data
+data = open(resource_path("res/settings.txt")).readlines()
+settings.load_enums_from_data(data)
+settings.load_fields_from_data(data)
+
+# Create param
 param = Param(None, settings)
 directory = "res/base_params/"
 
@@ -20,5 +36,7 @@ for filename in os.listdir(directory):
         data = file.read()
         print(f"Loading param file: {filename}")
         param.load_from_data(data)
+        print(f"Converting param file to bytes: {filename}")
+        param.to_bytes()
 
 print("Tests passed")
