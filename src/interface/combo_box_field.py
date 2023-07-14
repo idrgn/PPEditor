4f08f4f258b108543f240f6c0406f4e0bb6409e5
‎Maikel <37042855+idrgn@users.noreply.github.com>
2023-07-14 08:24:46 +0200

    Removed threading from comboBox

--- src/interface/combo_box_field.py
+++ src/interface/combo_box_field.py
@@ -2,30 +2,15 @@ import typing
 
 from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtGui import QStandardItem, QStandardItemModel
-from PyQt5.QtWidgets import (QComboBox, QHeaderView, QListView, QTableView,
-                             QWidget)
+from PyQt5.QtWidgets import QComboBox, QHeaderView, QListView, QTableView, QWidget
 
 from param.param import Field
-
-
-class PopulateThread(QThread):
-    finished = pyqtSignal()
-
-    def __init__(self, combo_box):
-        super().__init__()
-        self.combo_box = combo_box
-
-    def run(self):
-        self.combo_box.addItems(self.combo_box.field.settings.enum.get_values())
-        self.finished.emit()
 
 
 class QComboBoxField(QComboBox):
     def __init__(self, parent: typing.Optional[QWidget] = ...) -> None:
         super().__init__(parent)
         self.field = None
-        self.populate_thread = PopulateThread(self)
-        self.populate_thread.finished.connect(self.after_population)
         self.setDisabled(True)
         self.setView(QListView())
 
@@ -36,16 +21,13 @@ class QComboBoxField(QComboBox):
     def set_field(self, field: Field):
         if field.settings.enum:
             self.field = field
-
-            self.populate_thread.start()
-
-    def after_population(self):
-        self.currentTextChanged.connect(self.update_field_value)
-        self.setDisabled(False)
-        if self.field.settings.enum.null_value is not None:
-            self.setCurrentIndex(self.field.value + 1)
-        else:
-            self.setCurrentIndex(self.field.value)
+            self.addItems(field.settings.enum.get_values())
+            self.setDisabled(False)
+            if self.field.settings.enum.null_value is not None:
+                self.setCurrentIndex(self.field.value + 1)
+            else:
+                self.setCurrentIndex(self.field.value)
+            self.currentTextChanged.connect(self.update_field_value)
 
     def update_field_value(self, *args):
         if self.field is not None:
