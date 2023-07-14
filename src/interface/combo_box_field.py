891002e0e205924451c7174a953476cb0cc87b36
‎Maikel <37042855+idrgn@users.noreply.github.com>
2023-07-14 08:53:29 +0200

    Added popout combobox for long enums

--- src/interface/combo_box_field.py
+++ src/interface/combo_box_field.py
@@ -1,8 +1,13 @@
 import typing
 
-from PyQt5.QtCore import QThread, pyqtSignal
-from PyQt5.QtGui import QStandardItem, QStandardItemModel
-from PyQt5.QtWidgets import QComboBox, QHeaderView, QListView, QTableView, QWidget
+from PyQt5.QtWidgets import (
+    QComboBox,
+    QDialog,
+    QListView,
+    QListWidget,
+    QVBoxLayout,
+    QWidget,
+)
 
 from param.param import Field
 
@@ -11,23 +16,34 @@ class QComboBoxField(QComboBox):
     def __init__(self, parent: typing.Optional[QWidget] = ...) -> None:
         super().__init__(parent)
         self.field = None
+        self.is_long = False
         self.setDisabled(True)
         self.setView(QListView())
-
-    def add_options(self, options: list):
-        for i in range(len(options) - 1):
-            self.addItem(f"{i} ({options[i]})")
 
     def set_field(self, field: Field):
         if field.settings.enum:
             self.field = field
-            self.addItems(field.settings.enum.get_values())
-            self.setDisabled(False)
-            if self.field.settings.enum.null_value is not None:
-                self.setCurrentIndex(self.field.value + 1)
+            entry_count = len(field.settings.enum.get_values())
+            if entry_count > 50:
+                self.set_values_long()
             else:
-                self.setCurrentIndex(self.field.value)
-            self.currentTextChanged.connect(self.update_field_value)
+                self.set_values_short()
+            self.setDisabled(False)
+
+    def set_values_short(self):
+        self.addItems(self.field.settings.enum.get_values())
+        if self.field.settings.enum.null_value is not None:
+            self.setCurrentIndex(self.field.value + 1)
+        else:
+            self.setCurrentIndex(self.field.value)
+        self.currentTextChanged.connect(self.update_field_value)
+
+    def set_values_long(self):
+        index = self.field.value
+        if self.field.settings.enum.null_value is not None:
+            index += 1
+        self.addItem(self.field.settings.enum.get_values()[index])
+        self.is_long = True
 
     def update_field_value(self, *args):
         if self.field is not None:
@@ -36,3 +52,45 @@ class QComboBoxField(QComboBox):
                 self.field.set_value(index - 1)
             else:
                 self.field.set_value(index)
+
+    def showPopup(self) -> None:
+        if not self.is_long:
+            return super().showPopup()
+
+        index = self.field.value
+
+        if self.field.settings.enum.null_value is not None:
+            index += 1
+
+        list_dialog = ListDialog(self.field.settings.enum.get_values(), index)
+        if list_dialog.exec_() == QDialog.Accepted:
+            selected_item, selected_index = list_dialog.get_selected_item()
+            if selected_item == None and selected_index == -1:
+                return
+            self.setItemText(0, selected_item)
+            if self.field.settings.enum.null_value is not None:
+                self.field.set_value(selected_index - 1)
+            else:
+                self.field.set_value(selected_index)
+
+
+class ListDialog(QDialog):
+    def __init__(self, items: list, selected: int = 0):
+        super().__init__()
+        self.setWindowTitle("Select an entry")
+        self.list_widget = QListWidget(self)
+        self.list_widget.addItems(items)
+        self.list_widget.itemDoubleClicked.connect(self.accept)
+        # self.list_widget.setCurrentIndex(selected)
+        layout = QVBoxLayout()
+        layout.addWidget(self.list_widget)
+        self.setLayout(layout)
+
+    def get_selected_item(self):
+        selected_items = self.list_widget.selectedItems()
+        if selected_items:
+            selected_item = selected_items[0].text()
+            selected_index = self.list_widget.row(selected_items[0])
+            return selected_item, selected_index
+        else:
+            return None, -1
